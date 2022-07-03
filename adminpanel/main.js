9b4d74fc7bc4e009dfc4623277b72dfa60062919
thedevelopernag <70878074+thedevelopernag@users.noreply.github.com>
2022-07-03 19:47:04 +0530

    Add files via upload

---
+++ adminpanel/main.js
@@ -0,0 +1,19 @@
+
+
+
+       //--------------------toggle sidebar --------------------------------------------
+
+       let sidebar = document.querySelector(".sidebar");
+       let main = document.querySelector("main");
+       function togside() {
+           sidebar.classList.toggle("show");
+           main.classList.toggle("move")
+       }
+
+       let cut=document.querySelector(".cut");
+       cut.addEventListener("click",()=>{
+           sidebar.classList.remove("show");
+           main.classList.remove("move")
+       })
+
+     
